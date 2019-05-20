595aa0bd4db9c64bd120f7f252f8511107e1f827
Julio Lima <juliosergil@gmail.com>
2019-05-20 13:06:47 -0300

    Initial commit

---
+++ src/main/java/com/inpi/InpiRestFullApiApplication.java
@@ -0,0 +1,14 @@
+package com.inpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpiRestFullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InpiRestFullApiApplication.class, args);
+		int soma = 1;
+	}
+
+}
